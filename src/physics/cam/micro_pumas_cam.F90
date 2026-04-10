29439fd65b0c13653ddff9be989516c695e22188
Kanishk Gohil <kgohil@derecho4.hsn.de.hpc.ucar.edu>
2026-04-09 18:12:08 -0600

    PUMAS updates including SIP via ice-ice collision

--- src/physics/cam/micro_pumas_cam.F90
+++ src/physics/cam/micro_pumas_cam.F90
@@ -83,6 +83,9 @@ real(r8) :: micro_mg_max_nicons        = unset_r8  ! max allowed ice number conc
 
 logical, public :: do_cldliq ! Prognose cldliq flag
 logical, public :: do_cldice ! Prognose cldice flag
+! +KG
+logical, public :: do_icecol ! Prognose ice-ice collision flag
+! -KG
 
 integer :: num_steps ! Number of MG substeps
 
@@ -162,6 +165,12 @@ integer :: &
    relvar_idx,         &
    cmeliq_idx,         &
    accre_enhan_idx
+
+! Fields for UNICON
+integer :: &
+     am_evp_st_idx,      &! Evaporation area of stratiform precipitation
+     evprain_st_idx,     &! Evaporation rate of stratiform rain [kg/kg/s]. >= 0.
+     evpsnow_st_idx       ! Evaporation rate of stratiform snow [kg/kg/s]. >= 0.
 
 ! Fields needed as inputs to COSP
 integer :: &
@@ -247,6 +256,9 @@ subroutine micro_pumas_cam_readnl(nlfile)
 
   ! Namelist variables
   logical :: micro_mg_do_cldice = .true. ! do_cldice = .true., MG microphysics is prognosing cldice
+  ! +KG
+  logical :: micro_mg_do_icecol = .false. ! do_icecol = .false., MG microphysics is prognosing ice-ice collision
+  ! -KG
   logical :: micro_mg_do_cldliq = .true. ! do_cldliq = .true., MG microphysics is prognosing cldliq
   integer :: micro_mg_num_steps = 1      ! Number of substepping iterations done by MG (1.5 only for now).
 
@@ -256,7 +268,10 @@ subroutine micro_pumas_cam_readnl(nlfile)
   character(len=*), parameter :: sub = 'micro_pumas_cam_readnl'
 
   namelist /micro_mg_nl/ micro_mg_version, micro_mg_sub_version, &
-       micro_mg_do_cldice, micro_mg_do_cldliq, micro_mg_num_steps, &
+       ! +KG
+       micro_mg_do_cldice, micro_mg_do_icecol, &
+       micro_mg_do_cldliq, micro_mg_num_steps, &
+       ! -KG
        microp_uniform, micro_mg_dcs, micro_mg_precip_frac_method, &
        micro_mg_berg_eff_factor, micro_do_sb_physics, micro_mg_adjust_cpt, &
        micro_mg_do_hail, micro_mg_do_graupel, micro_mg_ngcons, micro_mg_ngnst, &
@@ -288,6 +303,9 @@ subroutine micro_pumas_cam_readnl(nlfile)
 
      ! set local variables
      do_cldice = micro_mg_do_cldice
+     ! +KG
+     do_icecol = micro_mg_do_icecol
+     ! -KG
      do_cldliq = micro_mg_do_cldliq
      num_steps = micro_mg_num_steps
 
@@ -348,6 +366,11 @@ subroutine micro_pumas_cam_readnl(nlfile)
 
   call mpi_bcast(do_cldice, 1, mpi_logical, mstrid, mpicom, ierr)
   if (ierr /= 0) call endrun(sub//": FATAL: mpi_bcast: do_cldice")
+
+  ! +KG
+  call mpi_bcast(do_icecol, 1, mpi_logical, mstrid, mpicom, ierr)
+  if (ierr /= 0) call endrun(sub//": FATAL: mpi_bcast: do_icecol")
+  ! -KG
 
   call mpi_bcast(do_cldliq, 1, mpi_logical, mstrid, mpicom, ierr)
   if (ierr /= 0) call endrun(sub//": FATAL: mpi_bcast: do_cldliq")
@@ -480,6 +503,9 @@ subroutine micro_pumas_cam_readnl(nlfile)
      write(iulog,*) '  micro_mg_version            = ', micro_mg_version
      write(iulog,*) '  micro_mg_sub_version        = ', micro_mg_sub_version
      write(iulog,*) '  micro_mg_do_cldice          = ', do_cldice
+     ! +KG
+     write(iulog,*) '  micro_mg_do_icecol          = ', do_icecol
+     ! -KG
      write(iulog,*) '  micro_mg_do_cldliq          = ', do_cldliq
      write(iulog,*) '  micro_mg_num_steps          = ', num_steps
      write(iulog,*) '  microp_uniform              = ', microp_uniform
@@ -654,6 +680,11 @@ subroutine micro_pumas_cam_register
    call pbuf_add_field('CC_nl',    'global',  dtype_r8, (/pcols,pver,dyn_time_lvls/), cc_nl_idx)
    call pbuf_add_field('CC_ni',    'global',  dtype_r8, (/pcols,pver,dyn_time_lvls/), cc_ni_idx)
    call pbuf_add_field('CC_qlst',  'global',  dtype_r8, (/pcols,pver,dyn_time_lvls/), cc_qlst_idx)
+
+   ! Fields for UNICON
+   call pbuf_add_field('am_evp_st',  'global', dtype_r8, (/pcols,pver/), am_evp_st_idx)
+   call pbuf_add_field('evprain_st', 'global', dtype_r8, (/pcols,pver/), evprain_st_idx)
+   call pbuf_add_field('evpsnow_st', 'global', dtype_r8, (/pcols,pver/), evpsnow_st_idx)
 
    ! Register subcolumn pbuf fields
    if (use_subcol_microp) then
@@ -896,7 +927,10 @@ subroutine micro_pumas_cam_init(pbuf2d)
            tmelt, latvap, latice, rhmini, &
            micro_mg_dcs,                  &
            micro_mg_do_hail,micro_mg_do_graupel, &
-           microp_uniform, do_cldice, use_hetfrz_classnuc, &
+           ! +KG
+           microp_uniform, &
+           do_cldice, do_icecol, use_hetfrz_classnuc, &
+           ! -KG
            micro_mg_precip_frac_method, micro_mg_berg_eff_factor, &
            micro_mg_accre_enhan_fact , &
            micro_mg_autocon_fact , micro_mg_autocon_nd_exp, micro_mg_autocon_lwp_exp, micro_mg_homog_size, &
@@ -1324,6 +1358,9 @@ subroutine micro_pumas_cam_init(pbuf2d)
       call pbuf_set_field(pbuf2d, acnum_idx,  0)
       call pbuf_set_field(pbuf2d, relvar_idx, 2._r8)
       call pbuf_set_field(pbuf2d, accre_enhan_idx, 1._r8)
+      call pbuf_set_field(pbuf2d, am_evp_st_idx,  0._r8)
+      call pbuf_set_field(pbuf2d, evprain_st_idx, 0._r8)
+      call pbuf_set_field(pbuf2d, evpsnow_st_idx, 0._r8)
       call pbuf_set_field(pbuf2d, prer_evap_idx,  0._r8)
       call pbuf_set_field(pbuf2d, bergso_idx, 0._r8)
       call pbuf_set_field(pbuf2d, icswp_idx, 0._r8)
@@ -1400,6 +1437,9 @@ subroutine micro_pumas_cam_tend(state, ptend, dtime, pbuf)
    real(r8), pointer :: npccn(:,:)     ! liquid activation number tendency
    real(r8), pointer :: rndst(:,:,:)
    real(r8), pointer :: nacon(:,:,:)
+   real(r8), pointer :: am_evp_st_grid(:,:)    ! Evaporation area of stratiform precipitation. 0<= am_evp_st <=1.
+   real(r8), pointer :: evprain_st_grid(:,:)   ! Evaporation rate of stratiform rain [kg/kg/s]
+   real(r8), pointer :: evpsnow_st_grid(:,:)   ! Evaporation rate of stratiform snow [kg/kg/s]
 
    real(r8), pointer :: prec_str(:)          ! [Total] Sfc flux of precip from stratiform [ m/s ]
    real(r8), pointer :: snow_str(:)          ! [Total] Sfc flux of snow from stratiform   [ m/s ]
@@ -2094,6 +2134,10 @@ subroutine micro_pumas_cam_tend(state, ptend, dtime, pbuf)
    call pbuf_get_field(pbuf, cmeliq_idx,      cmeliq_grid)
    call pbuf_get_field(pbuf, ast_idx,         ast_grid, start=(/1,1,itim_old/), kount=(/pcols,pver,1/))
 
+   call pbuf_get_field(pbuf, evprain_st_idx,  evprain_st_grid)
+   call pbuf_get_field(pbuf, evpsnow_st_idx,  evpsnow_st_grid)
+   call pbuf_get_field(pbuf, am_evp_st_idx,   am_evp_st_grid)
+
    !-----------------------------------------------------------------------
    !        ... Calculate cosine of zenith angle
    !            then cast back to angle (radians)
@@ -2699,7 +2743,10 @@ subroutine micro_pumas_cam_tend(state, ptend, dtime, pbuf)
       call subcol_field_avg(qme,       ngrdcol, lchnk, qme_grid)
       call subcol_field_avg(nevapr,    ngrdcol, lchnk, nevapr_grid)
       call subcol_field_avg(prain,     ngrdcol, lchnk, prain_grid)
+      call subcol_field_avg(evapsnow,  ngrdcol, lchnk, evpsnow_st_grid)
       call subcol_field_avg(bergso,    ngrdcol, lchnk, bergso_grid)
+
+      call subcol_field_avg(am_evp_st, ngrdcol, lchnk, am_evp_st_grid)
 
       ! Average fields which are not in pbuf
       call subcol_field_avg(qrout,     ngrdcol, lchnk, qrout_grid)
@@ -2796,6 +2843,9 @@ subroutine micro_pumas_cam_tend(state, ptend, dtime, pbuf)
       prain_grid      => prain
       bergso_grid     => bergso
 
+      am_evp_st_grid  = am_evp_st
+
+      evpsnow_st_grid = evapsnow
       qrout_grid      = qrout
       qsout_grid      = qsout
       nsout_grid      = nsout
@@ -3342,6 +3392,15 @@ subroutine micro_pumas_cam_tend(state, ptend, dtime, pbuf)
             fcti_grid(i)  = icecldf_grid(i,k)
             exit
          end if
+      end do
+   end do
+
+   ! Evaporation of stratiform precipitation fields for UNICON
+   evprain_st_grid(:ngrdcol,:pver) = nevapr_grid(:ngrdcol,:pver) - evpsnow_st_grid(:ngrdcol,:pver)
+   do k = top_lev, pver
+      do i = 1, ngrdcol
+         evprain_st_grid(i,k) = max(evprain_st_grid(i,k), 0._r8)
+         evpsnow_st_grid(i,k) = max(evpsnow_st_grid(i,k), 0._r8)
       end do
    end do
 
